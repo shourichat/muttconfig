8ff354b70f28a9232e0ea14b76f1707323d7f670
shourichat <64950530+shourichat@users.noreply.github.com>
2022-12-19 10:06:47 +0530

    Trial python file

---
+++ trial.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import datefinder
+
+with open('a', 'r') as file:
+    input_string = file.read().replace('\n', ' ')
+
+matches = datefinder.find_dates(input_string)
+
+print("Possible dates extracted from the email.")
+print("Choose the suitable one, or 0 if none.")
+k=1;
+matches = list(matches)
+print(matches)
+
+for match in matches:
+     print(k, ":", match)
+     k=k+1
+entry = input("Which one?\n")
+entry = int(entry)
+entry = entry-1
+
+print(matches[entry])
